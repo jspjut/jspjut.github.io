c5b80e533e0625231cd0fb5523fad183c7e98608
Josef Spjut <josef.spjut@gmail.com>
2017-07-26 21:00:50 -0400

    updated publish script to push to master.

--- scripts/publish2ghpages.sh
+++ scripts/publish2ghpages.sh
@@ -25,5 +25,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-# echo "Updating master branch"
-# cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)" && git push
